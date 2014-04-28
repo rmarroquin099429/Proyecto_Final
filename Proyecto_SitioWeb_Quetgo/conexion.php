21b2302180083f04594026c53415eddb18c29f5d
rmarroquin099429 <rudy.innov@gmail.com>
2014-04-28 15:24:13 -0600

    1. carpeta ingresada con los archivos del sitioweb

---
+++ Proyecto_SitioWeb_Quetgo/conexion.php
@@ -0,0 +1,36 @@
+﻿<?php 
+	
+	$nombre = $_POST["nombre"];
+	$apellido = $_POST["apellido"];
+	
+	$conect=mysql_connect('localhost','nomUsuario','123');
+	
+	// $queryStr = 'SELECT * FROM mydiagramabd.cliente';
+	if (!$conect) 
+	{
+	    echo 'No se pudo ejecutar la consulta: ' . mysql_error();
+	    exit;
+	}
+	
+	$insertar="INSERT INTO mydiagramabd.Cliente (Nombre, Apellido)
+	VALUES ('$nombre', '$apellido')";
+
+	if (!mysqli_query($conect,$insertar)) 
+		{
+		  die('Error: ' . mysqli_error($conect));
+		}
+	echo "1 registro agregado exitoasamente";
+
+
+	mysqli_close($conect);
+
+	// $query = mysql_query($queryStr);
+
+	// $filas=mysql_fetch_row($query);
+	// echo $filas[0];
+	echo 'Conectado satisfactoriamente';
+
+	mysql_close($conect);
+	
+	// print_r($filas);
+ ?>
